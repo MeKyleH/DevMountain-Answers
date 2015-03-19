3b6bd1d7faf6ee4bac7f8d033b44cc8e2bf727bb
MeKyleH <kyle.a.holt@gmail.com>
2015-03-18 19:33:59 -0600

    Create me.js

---
+++ me.js
@@ -0,0 +1,8 @@
+//Create an object called me. Give it a key of name with the value being your name, and another key of age with the value being your age. Then alert your name using dot notation.
+//code here
+var me = {
+	name: "Kyle Holt",
+	age: "26",
+};
+
+alert(me.name);
