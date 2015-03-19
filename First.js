a1d369367b5d2317cab78e478aeb68273b093648
MeKyleH <kyle.a.holt@gmail.com>
2015-03-18 19:36:00 -0600

    Create First.js

---
+++ First.js
@@ -0,0 +1,10 @@
+var arr = [1,2,3];
+
+//create a function named 'first' that returns the first object from a given array
+//alert the result of your function
+
+function first(yourArray) {
+	return yourArray[0];
+}
+
+alert(first(arr));
